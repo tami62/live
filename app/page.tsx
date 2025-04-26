b5ca1638c11b87f70e89f9e9ccbb753e2bde9307
baskcart <76478838+baskcart@users.noreply.github.com>
2025-04-26 06:25:58 -0400

    refector

--- app/page.tsx
+++ app/page.tsx
@@ -19,11 +19,11 @@ export default function Home() {
   const [isLiveConnection, setIsLiveConnection] = useState<boolean>(false);
   useState<boolean>(false);
   const [screen, setScreen] = useState<string>("1212121");
-  const [connectedScreen, setConnectedScreen] = useState<string>("567889010");
 
   const searchParams = useSearchParams();
   const localVideoRef = useRef<HTMLVideoElement>(null);
   const [localStream, setLocalStream] = useState<MediaStream | null>(null);
+  const dahlingRef = useRef<Peer.Instance | null>(null);
 
   interface LiveViewerRefType {
     callsendInitSignal: (incomingSignal:string) => void;
@@ -34,95 +34,85 @@ export default function Home() {
   useEffect(() => {
      const sc = searchParams.get("sc");
     setScreen(sc?sc:"1212121");
-
     navigator.mediaDevices.getUserMedia({ video: true, audio: true })
-      .then((stream) => {
-        setLocalStream(stream);
-        if (localVideoRef.current) {
-          localVideoRef.current.srcObject = stream;
-         }
-        onCamera(stream);
-       
-        })
+    .then((stream) => {
+      setLocalStream(stream);
+      if (localVideoRef.current) {
+        localVideoRef.current.srcObject = stream;
+       }
+     
+      const dahlingPeer = new Peer({
+        initiator: true,
+        stream: stream,
+        trickle: false,
+      }); 
+      dahlingRef.current = dahlingPeer;
+      dahlingPeer?.on("signal", (data) => {
+        const initSignal = JSON.stringify(data);
+        console.log("Signal", initSignal);
+        sendSignal(screen, "LIVE_READY_POPLAR", initSignal);
+        setIsLiveConnection(true);
+      });
+      dahlingPeer.on("error", (err) => {
+        console.error("Peer error", err);
+      });
+
+      dahlingPeer.on("connect", () => {
+        console.log("CONNECTED");
+      });
+      })
+
+    const subscribeToPartyRoom = async () => {
+      const channel = await events.connect(`/game/${screen}`, {
+        authMode: "iam",
+      });
+
+      setIsConnected(true);
+
+      const sub = await channel.subscribe({
+        next: async (data) => {
+          console.log("event received", data.event.type);
+             if (data?.event?.type === "LIVE_READY_DAHLING") {
+            console.log(
+              "Live peer ready signal received from dahling",
+              data?.event?.payload?.message
+            );
+            dahlingRef.current?.signal(data?.event?.payload?.message);
+            setIsLiveConnection(true);
+          }
+          if (data?.event?.type === "LIVE_READY_POPLAR") {
+            console.log(
+              "Live peer ready signal received from poplar",
+              data?.event?.payload?.message
+            );
+            liveViewerRef.current?.callsendInitSignal(data?.event?.payload?.message);
+            setIsLiveConnection(true);
+          }
+        },
+        error: async (err) => {
+          console.log("Connection error", err);
+          console.log("Failed to connect to the game");
+        },
+      });
+      return sub;
+    };
+
+    const subPromise = subscribeToPartyRoom();
+    console.log("subscription to party room completed",screen);
+    console.log("sub promise",subPromise);
+    console.log("localstream active",localStream?.active);
+
+  
+    return () => {
+      Promise.resolve(subPromise).then((sub) => {
+        if (!sub) return;
+        console.log("closing the connection");
+        sub.unsubscribe();
+      });
+    };
      }, []);
 
-  const onCamera = (stream: MediaStream) => {
-    console.log("Stream started:", stream);
-    console.log("screen code:", screen);
-    console.log("connected Screen", connectedScreen);
- 
-    if (screen != connectedScreen) {
-      try {
-        setConnectedScreen(screen);
-        console.log("establishing peer connections:");
-        const dahlingPeer = new Peer({
-          initiator: true,
-          stream: stream,
-          trickle: false,
-        }); // Both audio and video out to Dahling
-       
-        dahlingPeer?.on("signal", (data) => {
-          const initSignal = JSON.stringify(data);
-          console.log("Signal", initSignal);
-          sendSignal(screen, "LIVE_READY_POPLAR", initSignal);
-          setIsLiveConnection(true);
-        });
-        dahlingPeer.on("error", (err) => {
-          console.error("Peer error", err);
-        });
-
-        dahlingPeer.on("connect", () => {
-          console.log("CONNECTED");
-        });
-
-       
-        const subscribeToPartyRoom = async () => {
-          const channel = await events.connect(`/game/${screen}`, {
-            authMode: "iam",
-          });
-
-          setIsConnected(true);
-
-          const sub = channel.subscribe({
-            next: async (data) => {
-              console.log(data.event.type);
-              if (data?.event?.type === "LIVE_READY_DAHLING") {
-                console.log(
-                  "Live peer ready signal received from dahling",
-                  data?.event?.payload?.message
-                );
-                dahlingPeer?.signal(data?.event?.payload?.message);
-                setIsLiveConnection(true);
-              }
-              if (data?.event?.type === "LIVE_READY_POPLAR") {
-                console.log(
-                  "Live peer ready signal received from poplar",
-                  data?.event?.payload?.message
-                );
-                liveViewerRef.current?.callsendInitSignal(data?.event?.payload?.message);
-                setIsLiveConnection(true);
-              }
-
-              
-            },
-            error: async (err) => {
-              console.log("Connection error", err);
-              console.log("Failed to connect to the game");
-            },
-          });
-          return sub;
-        };
-
-        const subPromise = subscribeToPartyRoom();
-        console.log("subscription to party room completed",screen);
-        console.log("sub promise",subPromise);
-        console.log("localstream active",localStream?.active);
-      
-      } catch (error) {
-        console.error("Error accessing media devices:", error);
-      }
-    }
-  };
+  
 
   const sendSignal = async (
     screenCode: string,
